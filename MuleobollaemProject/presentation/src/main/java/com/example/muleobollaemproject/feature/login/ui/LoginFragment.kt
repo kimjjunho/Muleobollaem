8ac9e967d6d50a0c7409347e2ae2c4d944720d69
kimjjunho <kimjjun1007@gmail.com>
2022-05-26 12:12:07 +0900

    LoginFragement 수정
    BaseFragment 적용

--- MuleobollaemProject/presentation/src/main/java/com/example/muleobollaemproject/feature/login/ui/LoginFragment.kt
+++ MuleobollaemProject/presentation/src/main/java/com/example/muleobollaemproject/feature/login/ui/LoginFragment.kt
@@ -6,41 +6,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.muleobollaemproject.feature.main.MainActivity
+import com.example.muleobollaemproject.R
+import com.example.muleobollaemproject.base.BaseFragment
+import com.example.muleobollaemproject.feature.main.ui.MainActivity
 import com.example.muleobollaemproject.databinding.FragmentLoginBinding
 import com.example.muleobollaemproject.feature.login.LoginBaseActivity
 
 
-class LoginFragment : Fragment() {
+class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
 
-    private lateinit var mBinding : FragmentLoginBinding
-    private val binding get() = mBinding
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        mBinding = FragmentLoginBinding.inflate(inflater,container,false)
-        val view = binding.root
+    override fun initView() {
         val loginBaseActivity = activity as LoginBaseActivity
 
-        mBinding.btnSign.setOnClickListener {
-            loginBaseActivity.fragmentChange()
+        binding.run {
+            btnSign.setOnClickListener {
+                loginBaseActivity.fragmentChange()
+            }
+            imageBtnSign.setOnClickListener {
+                loginBaseActivity.fragmentChange()
+            }
+            btnLogin.setOnClickListener {
+                startActivity(Intent(loginBaseActivity, MainActivity::class.java))
+                loginBaseActivity.finish()
+            }
         }
-        mBinding.imageBtnSign.setOnClickListener {
-            loginBaseActivity.fragmentChange()
-        }
-        mBinding.btnLogin.setOnClickListener {
-            startActivity(Intent(loginBaseActivity, MainActivity::class.java))
-            loginBaseActivity.finish()
-        }
-
-        return view
     }
-
-    companion object {
-
-    }
+    override fun observeEvent() {}
 }
