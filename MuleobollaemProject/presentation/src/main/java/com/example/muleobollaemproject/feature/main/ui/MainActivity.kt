5538ae6ce475c94514a82dee80b398b6e96664ce
kimjjunho <kimjjun1007@gmail.com>
2022-05-26 11:04:35 +0900

    MainActivity 수정
    BaseActivity 적용

--- MuleobollaemProject/presentation/src/main/java/com/example/muleobollaemproject/feature/main/ui/MainActivity.kt
+++ MuleobollaemProject/presentation/src/main/java/com/example/muleobollaemproject/feature/main/ui/MainActivity.kt
@@ -1,44 +1,45 @@
-package com.example.muleobollaemproject.feature.main
+package com.example.muleobollaemproject.feature.main.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.muleobollaemproject.R
 import com.example.muleobollaemproject.feature.see.SeeActivity
 import com.example.muleobollaemproject.WriteActivity
+import com.example.muleobollaemproject.base.BaseActivity
 import com.example.muleobollaemproject.databinding.ActivityMainBinding
 import com.example.muleobollaemproject.feature.login.LoginBaseActivity
+import com.example.muleobollaemproject.feature.main.MainAdapter
+import com.example.muleobollaemproject.feature.main.MainData
 import com.example.muleobollaemproject.setStatusBarColorBlack
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var mBinding : ActivityMainBinding
-    private val binding get() = mBinding
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         setStatusBarColorBlack()
 
         val mainArrayList = ArrayList<MainData>()
-        mainArrayList.add(MainData("김준호","2학년 1반","Timetable 에 오류가 났어요 ㅜㅜ 이게 누가 알려주\n" +
-                "실 수 있나요..? "))
+        mainArrayList.add(
+            MainData("김준호","2학년 1반","Timetable 에 오류가 났어요 ㅜㅜ 이게 누가 알려주\n" +
+                "실 수 있나요..? ")
+        )
         mainArrayList.add(MainData("장석연","2학년 2반","dd"))
         mainArrayList.add(MainData("손지원","2학년 3반","ㅇㅇ"))
         mainArrayList.add(MainData("최성현","2학년 4반","ㅎ"))
 
-        mBinding.iamgeBtnBack.setOnClickListener {
+        binding.iamgeBtnBack.setOnClickListener {
             startActivity(Intent(this, LoginBaseActivity::class.java))
             finish()
         }
-        mBinding.imageBtnWrite.setOnClickListener {
+        binding.imageBtnWrite.setOnClickListener {
             startActivity(Intent(this,WriteActivity::class.java))
         }
 
-        mBinding.rvMain.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
-        mBinding.rvMain.setHasFixedSize(true)
-        mBinding.rvMain.adapter = MainAdapter(mainArrayList,this)
+        binding.rvMain.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
+        binding.rvMain.setHasFixedSize(true)
+        binding.rvMain.adapter = MainAdapter(mainArrayList,this)
 
     }
 
@@ -48,4 +49,6 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("title",title)
         startActivity(intent)
     }
+
+    override fun observeEvent() {}
 }
