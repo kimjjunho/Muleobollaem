76e80a12b081992ab080f8bafd171b90b5ed62a0
kimjjunho <kimjjun1007@gmail.com>
2022-05-28 11:53:59 +0900

    init::MainUseCase

---
+++ MuleobollaemProject/domain/src/main/java/com/example/domain/usecase/MainUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.domain.usecase
+
+import com.example.domain.base.UseCase
+import com.example.domain.entity.MainEntity
+import com.example.domain.repository.MainRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class MainUseCase @Inject constructor(
+    private val mainRepository: MainRepository
+): UseCase<Unit, Flow<MainEntity>>() {
+    override suspend fun execute(data: Unit): Flow<MainEntity> = mainRepository.getExchange()
+}
