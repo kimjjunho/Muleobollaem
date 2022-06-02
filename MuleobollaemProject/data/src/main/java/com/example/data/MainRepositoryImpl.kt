05211e34728322831161a1ca0baa16a82cb50401
kimjjunho <kimjjun1007@gmail.com>
2022-06-02 12:07:50 +0900

    init::MainRepositoryImpl

---
+++ MuleobollaemProject/data/src/main/java/com/example/data/MainRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.example.data
+
+import com.example.data.local.toRoomEntity
+import com.example.data.remote.RemoteMainDataSource
+import com.example.data.remote.datasorce.LocalMainDataSource
+import com.example.data.remote.model.toEntity
+import com.example.data.util.OfflineCacheUtil
+import com.example.domain.entity.MainEntity
+import com.example.domain.repository.MainRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class MainRepositoryImpl @Inject constructor(
+    private val remoteMainDataSource: RemoteMainDataSource,
+    private val localMainDataSource: LocalMainDataSource
+): MainRepository {
+    override suspend fun getExchange(): Flow<MainEntity> =
+        OfflineCacheUtil<MainEntity>()
+            .remoteData { remoteMainDataSource.getMain().toEntity() }
+            .localData { localMainDataSource.getMain() }
+            .doOnNeedRefresh { localMainDataSource.updateMain(it.toRoomEntity()) }
+            .createFlow()
+}
