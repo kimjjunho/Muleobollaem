81e9911da89d91ada7fea9ce2b9f60c04d1ddc8f
kimjjunho <kimjjun1007@gmail.com>
2022-06-03 08:24:43 +0900

    init::WriteActivity

---
+++ MuleobollaemProject2/presentation/src/main/java/com/example/muleobollaemproject2/feature/write/WriteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.muleobollaemproject2.feature.write
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.muleobollaemproject2.R
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
