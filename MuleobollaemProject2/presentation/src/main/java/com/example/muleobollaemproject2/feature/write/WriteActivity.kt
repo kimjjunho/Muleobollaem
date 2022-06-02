23453cd15cac5eae05afa1c36dfd1b1f829a5d6b
kimjjunho <kimjjun1007@gmail.com>
2022-06-03 08:31:16 +0900

    init::WriteActivityFunction

--- MuleobollaemProject2/presentation/src/main/java/com/example/muleobollaemproject2/feature/write/WriteActivity.kt
+++ MuleobollaemProject2/presentation/src/main/java/com/example/muleobollaemproject2/feature/write/WriteActivity.kt
@@ -3,10 +3,21 @@ package com.example.muleobollaemproject2.feature.write
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.muleobollaemproject2.R
+import com.example.muleobollaemproject2.base.BaseActivity
+import com.example.muleobollaemproject2.databinding.ActivityWriteBinding
 
-class WriteActivity : AppCompatActivity() {
+class WriteActivity : BaseActivity<ActivityWriteBinding>(R.layout.activity_write) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write)
+        binding.run {
+            imageBtnFinish.setOnClickListener {
+                finish()
+            }
+            iamgeBtnBack.setOnClickListener {
+                finish()
+            }
+        }
     }
+
+    override fun observeEvent() {}
 }
