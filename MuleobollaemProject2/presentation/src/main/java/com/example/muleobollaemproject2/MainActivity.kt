fa93476b34bf18a7768cc099f4aacf8021925e04
kimjjunho <kimjjun1007@gmail.com>
2022-06-02 23:35:44 +0900

    init::MainAdapter

--- MuleobollaemProject2/presentation/src/main/java/com/example/muleobollaemproject2/MainActivity.kt
+++ MuleobollaemProject2/presentation/src/main/java/com/example/muleobollaemproject2/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.muleobollaemproject2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.muleobollaemproject2.base.BaseActivity
+import com.example.muleobollaemproject2.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity @Inject constructor(): BaseActivity<ActivityMainBinding>(
+    R.layout.activity_main
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setStatusBarColorBlack()
+
+        val mainArrayList = ArrayList<MainData>()
+        mainArrayList.add(
+            MainData(
+                0, "김준호", "2학년 1반", "Timetable 에 오류가 났어요 ㅜㅜ 이게 누가 알려주\n" +
+                        "실 수 있나요..? "
+            )
+        )
+        mainArrayList.add(MainData(1, "장석연", "2학년 2반", "dd"))
+        mainArrayList.add(MainData(2, "손지원", "2학년 3반", "ㅇㅇ"))
+        mainArrayList.add(MainData(3, "최성현", "2학년 4반", "ㅎ"))
+
+        binding.iamgeBtnBack.setOnClickListener {
+            startActivity(Intent(this, LoginBaseActivity::class.java))
+            finish()
+        }
+        binding.imageBtnWrite.setOnClickListener {
+            startActivity(Intent(this, WriteActivity::class.java))
+        }
+
+        binding.rvMain.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.rvMain.setHasFixedSize(true)
+        binding.rvMain.adapter = MainAdapter(mainArrayList, this)
+    }
+
+    fun moveSee(main: String, title: String) {
+        val intent = Intent(applicationContext, SeeActivity::class.java)
+        intent.putExtra("main", main)
+        intent.putExtra("title", title)
+        startActivity(intent)
+    }
+
+    override fun observeEvent() {
+
     }
 }
---
+++ MuleobollaemProject2/presentation/src/main/java/com/example/muleobollaemproject2/MainAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.muleobollaemproject2
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.muleobollaemproject2.databinding.ItemMainBinding
+
+class MainAdapter (private val mainList:ArrayList<MainData>, private val mainActivity: MainActivity):
+    RecyclerView.Adapter<MainAdapter.CustomViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_main,parent,false)
+
+        return CustomViewHolder(ItemMainBinding.bind(view))
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.binding.itemName.text = mainList[position].name
+        holder.binding.itemTitle.text = mainList[position].title
+        holder.binding.itemMain.text = mainList[position].main
+        holder.itemView.setOnClickListener {
+            mainActivity.moveSee(mainList[position].main, mainList[position].title)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return mainList.size
+    }
+
+    class CustomViewHolder(val binding: ItemMainBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(){
+        }
+    }
+}
