3471173d8367d16e9a94ba9d45427bc67512cec8
kimjjunho <kimjjun1007@gmail.com>
2022-06-03 22:14:03 +0900

    init::LocalMainDataSource

---
+++ MuleobollaemProject2/data/src/main/java/com/example/data/local/LocalMainDataSource.kt
@@ -0,0 +1,12 @@
+package com.example.data.local
+
+import com.example.data.local.entity.MainRoomEntity
+import com.example.domain.entity.MainEntity
+
+interface LocalMainDataSource {
+    suspend fun getMain(): MainEntity
+
+    suspend fun insertMain(mainRoomEntity: List<MainRoomEntity>)
+
+    suspend fun updateMain(mainRoomEntity: List<MainRoomEntity>)
+}
