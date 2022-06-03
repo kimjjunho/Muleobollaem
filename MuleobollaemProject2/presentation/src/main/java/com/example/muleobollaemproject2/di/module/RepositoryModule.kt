d4e3a3c426636def2455953ac811ade0b09b8f76
kimjjunho <kimjjun1007@gmail.com>
2022-06-04 01:39:14 +0900

    init::RepositoryModule

---
+++ MuleobollaemProject2/presentation/src/main/java/com/example/muleobollaemproject2/di/module/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.example.muleobollaemproject2.di.module
+
+import com.example.data.MainRepositoryImpl
+import com.example.domain.repository.MainRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun bindMainRepositoryImpl(
+        mainRepositoryImpl: MainRepositoryImpl
+    ): MainRepository
+
+}
