97dc857b443a9b165e563c8fc4eecb617678776d
kimjjunho <kimjjun1007@gmail.com>
2022-06-03 23:14:09 +0900

    init::MainDataSourceModule

---
+++ MuleobollaemProject2/presentation/src/main/java/com/example/muleobollaemproject2/di/module/main/MainDataSourceModule.kt
@@ -0,0 +1,32 @@
+package com.example.muleobollaemproject2.di.module.main
+
+import com.example.data.local.dao.MainDao
+import com.example.data.local.datasource.LocalMainDataSource
+import com.example.data.local.datasource.LocalMainDataSourceImpl
+import com.example.data.remote.datasource.RemoteMainDataSource
+import com.example.data.remote.datasource.RemoteMainDataSourceImpl
+import com.example.data.remote.network.MainAPI
+import com.example.domain.handler.MainErrorHandler
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.xml.sax.ErrorHandler
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MainDataSourceModule {
+    @Singleton
+    @Provides
+    fun provideMainRemoteDataSource(
+        mainAPI: MainAPI,
+        errorHandler: MainErrorHandler
+    ):RemoteMainDataSource = RemoteMainDataSourceImpl(mainAPI, errorHandler)
+
+    @Singleton
+    @Provides
+    fun provideMainDataSource(
+        mainDao: MainDao
+    ) : LocalMainDataSource = LocalMainDataSourceImpl(mainDao)
+}
