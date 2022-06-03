3506d4e26e1783ed42a29336d169d5ad368cd5d5
kimjjunho <kimjjun1007@gmail.com>
2022-06-03 22:24:46 +0900

    init::MainDatabase

---
+++ MuleobollaemProject2/data/src/main/java/com/example/data/local/database/MainDatabase.kt
@@ -0,0 +1,15 @@
+package com.example.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.data.local.dao.MainDao
+import com.example.data.local.entity.MainRoomEntity
+
+@Database(
+    entities = [MainRoomEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class MainDatabase: RoomDatabase() {
+    abstract fun mainDao(): MainDao
+}
