511fb64fab61710a6e7df133e6ff582745c6b569
kimjjunho <kimjjun1007@gmail.com>
2022-06-03 22:10:16 +0900

    init::MainRoomEntity

---
+++ MuleobollaemProject2/data/src/main/java/com/example/data/local/entity/MainRoomEntity.kt
@@ -0,0 +1,28 @@
+package com.example.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.domain.entity.Main
+import com.example.domain.entity.MainEntity
+
+@Entity
+data class MainRoomEntity(
+    val id_pk:Int,
+    val name: String,
+    val title: String,
+    val main: String
+){
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+}
+
+fun Main.toRoomEntity() =
+    MainRoomEntity(
+        id_pk,
+        name,
+        title,
+        main
+    )
+
+
+fun MainEntity.toRoomEntity() = this.mainList.map { it.toRoomEntity() }
+
